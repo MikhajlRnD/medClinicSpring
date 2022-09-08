d7487710e06150f39f3e90a2929176c4c4b9c21b
MikhajlRnD <Mikhajl-b@yandex.ru>
2022-09-08 11:24:19 +0300

    Initial commit

---
+++ src/main/java/ru/bakulyerov/medClinicSpring/MedClinicSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.bakulyerov.medClinicSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedClinicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedClinicSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bakulyerov/medClinicSpring/MedClinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bakulyerov.medClinicSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedClinicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
